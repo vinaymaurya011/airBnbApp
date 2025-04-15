bad9486a0af784e10f2022deba0f09aa4c03cd98
vinay maurya <vinaymaurya06072000@gmail.com>
2025-04-16 00:01:32 +0530

    Added further updates

--- src/main/java/com/techboot/projects/airBnbApp/controller/HotelBookingController.java
+++ src/main/java/com/techboot/projects/airBnbApp/controller/HotelBookingController.java
@@ -18,7 +18,7 @@ public class HotelBookingController {
 
     private final BookingService bookingService;
 
-    @PostMapping
+    @PostMapping("/init")
     public ResponseEntity<BookingDto> initialiseBooking(@RequestBody BookingRequest bookingRequest){
         return ResponseEntity.ok(bookingService.initialiseBooking(bookingRequest));
 
